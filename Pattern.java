66cd899e3c8933c62e337b06f1c6f4685ab3699a
SuprimTamang <suprimtamang123456789@gmail.com>
2023-12-12 23:15:08 +0000

    pattern

---
+++ Pattern.java
@@ -0,0 +1,14 @@
+
+class Pattern {
+    public static void main (String[]args){
+    //outer loop
+       for (int i=1 ; i< 5; i++ ){
+        //inner loop 
+        for(int j =1 ; j <6; j++){
+            System.out.print("*");
+        }
+      
+        System.out.println();
+       }
+    }
+}
